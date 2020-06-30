510909aa49c622fb86b327994424db97bd93053e
李清波 <nnd3@qq.com>
2020-06-30 16:43:39 +0800

    请求相关代码提取重构

--- src/main/java/com/github/rwocj/wx/service/WxPayV3Service.java
+++ src/main/java/com/github/rwocj/wx/service/WxPayV3Service.java
@@ -101,36 +101,9 @@ public class WxPayV3Service {
         }
 
         OrderType orderType = createOrderRequest.getOrderType();
-        Request request = new Request.Builder()
-                .url(ORDER_URL + orderType.getUrl())
-                .post(RequestBody.create(MediaType.parse("application/json;charset=utf-8"),
-                        objectMapper.writeValueAsString(createOrderRequest)))
-                .build();
-        try {
-            Response execute = okHttpClient.newCall(request).execute();
-            ResponseBody body = execute.body();
-
-            if (body != null) {
-                String string = body.string();
-                log.debug("下单请求响应：{}", string);
-                if (execute.isSuccessful()) {
-                    Headers headers = execute.headers();
-                    boolean validate = validator.validate(new OkHttpWxHeaders(execute), string);
-                    if (!validate) {
-                        throw new WxPayException(String.format("验证响应失败!:响应体：%s,响应headers:%s", string, buildHeader(headers)));
-                    } else {
-                        return objectMapper.readTree(string).get("prepay_id").asText();
-                    }
-                } else {
-                    throw new WxPayException(string);
-                }
-            } else {
-                throw new WxPayException("下单请求响应为空!");
-            }
-        } catch (IOException e) {
-            throw new WxPayException("微信支付下单失败：", e);
-        }
-
+        String url = ORDER_URL + orderType.getUrl();
+        String res = request(url, createOrderRequest);
+        return objectMapper.readTree(res).get("prepay_id").asText();
     }
 
 
@@ -168,35 +141,8 @@ public class WxPayV3Service {
             validateOrderRequest(refundRequest);
         }
 
-        Request request = new Request.Builder()
-                .url(REFUND_URL)
-                .post(RequestBody.create(MediaType.parse("application/json;charset=utf-8"),
-                        objectMapper.writeValueAsString(refundRequest)))
-                .build();
-        try {
-            Response execute = okHttpClient.newCall(request).execute();
-            ResponseBody body = execute.body();
-            if (body != null) {
-                String string = body.string();
-                log.debug("退款请求响应：{}", string);
-                if (execute.isSuccessful()) {
-                    Headers headers = execute.headers();
-                    boolean validate = validator.validate(new OkHttpWxHeaders(execute), string);
-                    if (!validate) {
-                        throw new WxPayException(String.format("验证响应失败!:响应体：%s,响应headers:%s", string, buildHeader(headers)));
-                    } else {
-                        return objectMapper.readValue(string, WxRefundRes.class);
-                    }
-                } else {
-                    throw new WxPayException(string);
-                }
-            } else {
-                throw new WxPayException("退款请求响应为空!");
-            }
-        } catch (IOException e) {
-            throw new WxPayException("微信退款失败：", e);
-        }
-
+        String res = request(REFUND_URL, refundRequest);
+        return objectMapper.readValue(res, WxRefundRes.class);
     }
 
     /**
@@ -256,6 +202,40 @@ public class WxPayV3Service {
                 FieldError fieldError = (FieldError) objectError;
                 return new ValidateResult(fieldError.getObjectName(), fieldError.getField(), fieldError.getDefaultMessage(), fieldError.getRejectedValue());
             }).collect(toList()).toString());
+        }
+    }
+
+    @SneakyThrows(JsonProcessingException.class)
+    protected String request(String url, Object requestObject) throws WxPayException {
+        String content = objectMapper.writeValueAsString(requestObject);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(RequestBody.create(MediaType.parse("application/json;charset=utf-8"),
+                        content))
+                .build();
+        log.debug("微信请求体：{}", content);
+        try {
+            Response execute = okHttpClient.newCall(request).execute();
+            ResponseBody body = execute.body();
+            if (body != null) {
+                String string = body.string();
+                log.debug("微信请求响应：{}", string);
+                if (execute.isSuccessful()) {
+                    Headers headers = execute.headers();
+                    boolean validate = validator.validate(new OkHttpWxHeaders(execute), string);
+                    if (!validate) {
+                        throw new WxPayException(String.format("验证响应失败!:响应体：%s,响应headers:%s", string, buildHeader(headers)));
+                    } else {
+                        return string;
+                    }
+                } else {
+                    throw new WxPayException(string);
+                }
+            } else {
+                throw new WxPayException("微信请求响应为空!");
+            }
+        } catch (IOException e) {
+            throw new WxPayException("微信请求失败：", e);
         }
     }
 

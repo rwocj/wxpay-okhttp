e681a05fa8c52b3ad88512ea580da1f51d9e5db6
李清波 <1991tss@gmail.com>
2024-07-05 14:43:19 +0800

    fix: 《申请退款API》新增字段 from（退款出资账户及金额）

--- src/main/java/top/rwocj/wx/dto/WxRefundRequest.java
+++ src/main/java/top/rwocj/wx/dto/WxRefundRequest.java
@@ -240,5 +240,21 @@ public class WxRefundRequest {
          */
         @JsonProperty("amount")
         private Integer amount;
+
+        public String getAccount() {
+            return account;
+        }
+
+        public void setAccount(String account) {
+            this.account = account;
+        }
+
+        public Integer getAmount() {
+            return amount;
+        }
+
+        public void setAmount(Integer amount) {
+            this.amount = amount;
+        }
     }
 }
